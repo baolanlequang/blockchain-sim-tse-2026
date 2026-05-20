f8fff4b9c63826db9e3fe99cf356ccffafcdd7ce
baolan2005 <baolan2005@gmail.com>
2026-05-20 12:25:01 +0200

    update simulator

--- trilemma/run_selfish_job.sh
+++ trilemma/run_selfish_job.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=run_selfishmining_job  # Name of the job
 #SBATCH --output=result_%j.out            # Standard output log (%j inserts JobID)
 #SBATCH --error=result_%j.err             # Error log
-#SBATCH --nodes=20                        # Number of nodes
-#SBATCH --ntasks=20                        # Number of tasks
+#SBATCH --nodes=5                        # Number of nodes
+#SBATCH --ntasks=5                        # Number of tasks
 #SBATCH --cpus-per-task=20                # Number of CPU cores per task
 #SBATCH --tasks-per-node=50
 #SBATCH --mem=360000                      # Total memory (RAM)
