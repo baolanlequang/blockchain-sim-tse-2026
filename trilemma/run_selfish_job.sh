47d19f502340f19900755e9f3081c70255067019
baolan2005 <baolan2005@gmail.com>
2026-05-20 12:26:46 +0200

    update simulator

--- trilemma/run_selfish_job.sh
+++ trilemma/run_selfish_job.sh
@@ -3,11 +3,10 @@
 #SBATCH --output=result_%j.out            # Standard output log (%j inserts JobID)
 #SBATCH --error=result_%j.err             # Error log
 #SBATCH --nodes=1                        # Number of nodes
-#SBATCH --ntasks=10                        # Number of tasks
+#SBATCH --ntasks=1                        # Number of tasks
 #SBATCH --cpus-per-task=20                # Number of CPU cores per task
-#SBATCH --tasks-per-node=50
 #SBATCH --mem=360000                      # Total memory (RAM)
 #SBATCH --time=72:00:00                   # Time limit (HH:MM:SS)
 #SBATCH --partition=cpu                   # Partition/Queue name
 
-java -Xmx240G -XX:ParallelGCThreads=20 -jar selfishmining.jar org.palladiosimulator.blockchainsystems.trilemma/optimized_deterministic_lhs_configurations.csv org.palladiosimulator.blockchainsystems.trilemma/testmodels org.palladiosimulator.blockchainsystems.trilemma/testmodels/configuration.json
+java -Xmx300G -XX:ParallelGCThreads=20 -jar selfishmining.jar org.palladiosimulator.blockchainsystems.trilemma/optimized_deterministic_lhs_configurations.csv org.palladiosimulator.blockchainsystems.trilemma/testmodels org.palladiosimulator.blockchainsystems.trilemma/testmodels/configuration.json
