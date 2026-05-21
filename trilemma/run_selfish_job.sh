7f8c5b6f6004d48a73edd9f066d242209e1256d3
baolan2005 <baolan2005@gmail.com>
2026-05-21 10:14:12 +0200

    update simulator

--- trilemma/run_selfish_job.sh
+++ trilemma/run_selfish_job.sh
@@ -4,14 +4,14 @@
 #SBATCH --error=result_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48
-#SBATCH --mem=1000000
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=500000
 #SBATCH --time=72:00:00
 #SBATCH --partition=highmem
 
-java -Xms900G -Xmx900G \
+java -Xms400G -Xmx400G \
      -XX:+UseG1GC \
-     -XX:ParallelGCThreads=48 \
+     -XX:ParallelGCThreads=24 \
      -XX:+HeapDumpOnOutOfMemoryError \
      -XX:HeapDumpPath=heapdump_${SLURM_JOB_ID}.hprof \
      -jar selfishmining.jar \
