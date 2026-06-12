e81546f7d9f106c772845474d117edb6d72abc98
baolan2005 <baolan2005@gmail.com>
2026-06-12 22:42:15 +0200

    update analysis

--- 3sim/org.palladiosimulator.blockchainsystems.threesim/target/classes/org/palladiosimulator/blockchainsystems/threesim/creation/ThreesimMiningProcessFactory.kt
+++ 3sim/org.palladiosimulator.blockchainsystems.threesim/target/classes/org/palladiosimulator/blockchainsystems/threesim/creation/ThreesimMiningProcessFactory.kt
@@ -23,20 +23,11 @@ class ThreesimMiningProcessFactory(
 
     val nodeResourcePowerShare = nodeResourcePower / resourcePowerCalculator.calculateGlobalResourcePower()
 
-    // meanBlockTime is specified in SECONDS, but the simulation clock runs in
-    // MILLISECONDS (network latency and transmission durations are in ms). Convert to
-    // ms here so block production and block propagation share the same time unit.
-    // Without this, mined blocks land on the clock ~1000x too early, making propagation
-    // appear far slower than block production -> pathological forking -> event explosion.
-    val nodeAverageBlockArrivalTimeMs = (meanBlockTime * MILLIS_PER_SECOND) / nodeResourcePowerShare
+    val nodeAverageBlockArrivalTime = meanBlockTime / nodeResourcePowerShare
 
     return MiningProcessImpl(
-      nodeAverageBlockArrivalTimeMs,
+      nodeAverageBlockArrivalTime,
       RandomGenerator.of("Random")
     )
-  }
-
-  companion object {
-    private const val MILLIS_PER_SECOND = 1000.0
   }
 }
